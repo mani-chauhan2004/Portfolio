e643cc170a2e89efc988e437e9737deb600e3ec9
mani-chauhan2004 <chauhan.mani1070@gmail.com>
2025-03-17 00:35:01 +0530

    Add NavIconsContainer and Animation components

--- portfolio-manaswee/src/components/NavBar.tsx
+++ portfolio-manaswee/src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import CircularText from './CircularText';
+import NavIconsContainer from './NavIconsContainer';
 function NavBar() {
   return (
-    <div className='flex items-center w-screen justify-between'>
+    <div className='flex z-100 items-center w-screen justify-between relative px-4'>
         <CircularText
             text="MANASWEECHAUHAN"
             onHover="speedUp"
             spinDuration={20}
             className="custom-class"
         />
-        <div></div>
+        <NavIconsContainer/>
         <div></div>
     </div>
   )
---
+++ portfolio-manaswee/src/components/NavIconsContainer.tsx
@@ -0,0 +1,14 @@
+import ShinyText from "./ShinyText";
+
+function NavIconsContainer() {
+  return (
+    <div className="flex absolute left-1/4 bg-neutral-800 w-6/12 items-center justify-around p-4 rounded-xl">
+        <a className="text-2xl font-orbitron font-normal" href="#"><ShinyText text={"Home"} disabled={false} speed={5}></ShinyText></a>
+        <a className="text-2xl font-orbitron font-normal" href="#"><ShinyText text={"About"} disabled={false} speed={5}></ShinyText></a>
+        <a className="text-2xl font-orbitron font-normal" href="#"><ShinyText text={"Projects"} disabled={false} speed={5}></ShinyText></a>
+        <a className="text-2xl font-orbitron font-normal" href="#"><ShinyText text={"Contact"} disabled={false} speed={5}></ShinyText></a>
+    </div>
+  )
+}
+
+export default NavIconsContainer;
