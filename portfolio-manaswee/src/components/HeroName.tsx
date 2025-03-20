10c2d9cd070c910c6b53b0f8f2b24b578d670789
mani-chauhan2004 <chauhan.mani1070@gmail.com>
2025-03-20 21:43:48 +0530

    Fix hero image height section for mobile view

--- portfolio-manaswee/src/components/HeroName.tsx
+++ portfolio-manaswee/src/components/HeroName.tsx
@@ -3,7 +3,7 @@
 function HeroName() {
   return (
     <div className="cursor-default flex items-center justify-center flex-col md:flex-row">
-        <div className="relative w-[100vw] h-[100vh] md:h-[120vh] md:w-[40vw]">
+        <div className="relative w-[100vw] h-[60vh] md:h-[120vh] md:w-[40vw] overflow-y-scroll">
             <div className="absolute inset-0 bg-[#0d0d0d]/70 flex items-center justify-center flex-col">
                 {/* <StaggeredText words={["Resolver", "Creative", "Awesome"]}/> */}
                 <div className="pl-12">
@@ -15,7 +15,7 @@ function HeroName() {
                 </div>
             </div>
         </div>
-        <div className=' flex relative w-[100vw] h-[120vh] items-center gap-38 flex-col p-12 bg-[url("/IMG/hero_image.png")] bg-right bg-contain bg-no-repeat md:bg-cover md:bg-top md:w-[60vw]' id="Home">
+        <div className=' flex relative w-[100vw] h-[80vh] sm:h-[120vh] items-center gap-38 flex-col p-12 bg-[url("/IMG/hero_image.png")] bg-right bg-contain bg-no-repeat md:bg-cover md:bg-top md:w-[60vw]' id="Home">
             <div className=" absolute inset-0 bg-linear-to-b from-[#0d0d0d]/60 from-60% to-[#0d0d0d]/100"></div>
         </div>
     </div>
