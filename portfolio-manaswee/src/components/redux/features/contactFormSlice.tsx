863a4f60ebaedaadf742b2309859935cbe019c30
mani-chauhan2004 <chauhan.mani1070@gmail.com>
2025-04-26 18:46:18 +0530

    Add response log

--- portfolio-manaswee/src/components/redux/features/contactFormSlice.tsx
+++ portfolio-manaswee/src/components/redux/features/contactFormSlice.tsx
@@ -8,6 +8,7 @@ type formDataType = {
 export const sendMessage = createAsyncThunk<AxiosResponse<any, any>, formDataType, {rejectValue: any}>('contactForm/sendMessage', async (formData, { rejectWithValue }) => {
     try {
         const response = await axios.post(`http://localhost:8080/message/api/send-message`, formData);
+        console.log(response.data);
         return response.data;
     }catch(error: any) {
         return rejectWithValue(error.response?.data || 'Something went wrong');
